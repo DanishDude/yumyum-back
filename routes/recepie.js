cf3be4470f7748f00e5d47b7fa948e0dfffaaa4b
DanishDude <christian.hinge@gmail.com>
2019-11-05 08:22:34 +0100

    wip

--- routes/recepie.js
+++ routes/recepie.js
@@ -1,25 +1,51 @@
-import express from 'express';
-
+const express = require('express');
 const connection = require('../conf');
+const multer = require('multer');
 
 const router = express.Router();
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
+
+const acceptableFileTypes = ['image/jpeg', 'image/png'];
+const fileFilter = (req, file, cb) => {
+  if (acceptableFileTypes.includes(file.mimeType)) {
+    cb(null, true);
+  } else {
+    cb(new Error('Only .jpeg or .png files are accepted'), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  Limits: {
+    fileSize: 1024 * 1024 * 5 // 5MB
+  },
+  fileFilter
+});
 
 // Get all recepies
 router.get('/recepie', (req, res) => {
-  connection.query('SELECT * FROM recepie;', (err, results) => {
+  connection.query('SELECT * FROM recepie;', (err, recepies) => {
     if (err) {
       res.status(500).send('Ah Snap :-/');
     } else {
-      res.json(results);
+      res.json(recepies);
     }
   });
 });
 
 // Add recepie
-router.post('/recepie', (req, res) => {
-  const formData = req.body;
-  connection.query('INSERT INTO recepie SET ?', formData, (err, results) => {
+router.post('/recepie', upload.single('recepieImage'), (req, res) => {
+  console.log('REQ.FILE', req.file); // https://www.youtube.com/watch?v=srPXMt1Q0nY
+  connection.query('INSERT INTO recepie SET ?', req.body, (err, results) => {
     if (err) {
+      console.log(err);
       res.status(500).send('Ah Snap :-/');
     } else {
       res.json(results);
