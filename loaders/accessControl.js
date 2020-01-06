0392e342c4829e28e8f1635ab96a0104d4106027
Christian Hinge <christianhinge@iMac-de-Christian.local>
2020-01-06 17:16:53 +0100

    get user

--- loaders/accessControl.js
+++ loaders/accessControl.js
@@ -5,11 +5,15 @@ module.exports.hydrateReq = async (req, res, next) => {
   try {
     if (req.token) {
       const decoded = jwt.decode(req.token, privateKey);
-      req.user = {};
-      req.user.id = decoded.id;
-      req.user.email = decoded.email;
-      console.log(`INFO - ID: ${req.user.id}, EMAIL: ${req.user.email}`);
-    }
+      if (decoded) {
+        req.user = {};
+        req.user.id = decoded.id;
+        req.user.email = decoded.email;
+        console.log('USER', req.user);
+      } else {
+          console.log('No user detected');
+      };
+    };
   } catch (err) {
     next(err);
   }
